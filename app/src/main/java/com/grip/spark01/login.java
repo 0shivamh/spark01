c332f6cbb45e82df7302f281915fd059db70550d
aishivam <shivamhande0000@gmail.com>
2021-06-13 00:53:56 +0530

    working on valiadtion

--- app/src/main/java/com/grip/spark01/login.java
+++ app/src/main/java/com/grip/spark01/login.java
@@ -41,44 +41,50 @@ public class login extends AppCompatActivity {
         short_t=findViewById(R.id.stitle);
 
 
-        forgot.setOnClickListener((View)->{
-            Intent intent=new Intent(login.this,forgot.class);
-        startActivity(intent);
 
-//            Pair[] pairs=new Pair[4];
-//            pairs[0]=new Pair<View,String>(logo,"logoimg");
-//            pairs[1]=new Pair<View,String>(head,"head");
-//            pairs[2]=new Pair<View,String>(short_t,"t_t");
-//            pairs[3]=new Pair<View,String>(email,"email_t");
-//            pairs[4]=new Pair<View,String>(go,"go_t");
+
+
+//        private Boolean validateName(){
+//            String val=psw.getEditText().getText().toString();
+//            String pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
+//            if(val.isEmpty()){
+//                email.setError("Field cannot be empty");
+//                return false;
+//            }
+//            else if(val.matches(pattern)){
+//                email.setError("Invalid email address");
+//                return false;
+//            }
+//            else{
+//                email.setError(null);
+//                return true;
+//            }
+//        }
 //
-//
-//
-//            ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(login.this,pairs);
-//
-//            startActivity(intent,options.toBundle());
-        });
+//        private Boolean validatPSW(){
+//            String val=psw.getEditText().getText().toString();
+//            if(val.isEmpty()){
+//                psw.setError("Field cannot be empty");
+//                return false;
+//            }
+//            else{
+//                psw.setError(null);
+//                return true;
+//            }
+//        }
+
+
+        //call activity
 
         HandleSignUp.setOnClickListener((View)->{
             Intent intent=new Intent(login.this,signup.class);
             startActivity(intent);
-
-//                Pair[] pairs=new Pair[2];
-//
-//                pairs[0]=new Pair<View,String>(logo,"logoimg");
-//                pairs[1]=new Pair<View,String>(head,"head");
-//                pairs[2]=new Pair<View,String>(short_t,"t_t");
-//                pairs[3]=new Pair<View,String>(email,"email_t");
-//                pairs[4]=new Pair<View,String>(psw,"psw_t");
-//                pairs[5]=new Pair<View,String>(forgot,"forgot_t");
-//                pairs[6]=new Pair<View,String>(go,"go_t");
-//                pairs[7]=new Pair<View,String>(HandleSignUp,"n_t");
-//
-//
-//
-//                ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(login.this,pairs);
-//
-//                startActivity(intent,options.toBundle());
         });
+
+        forgot.setOnClickListener((View)->{
+            Intent intent=new Intent(login.this,forgot.class);
+            startActivity(intent);
+        });
+
 
 }}
--- app/src/main/java/com/grip/spark01/signup.java
+++ app/src/main/java/com/grip/spark01/signup.java
@@ -3,15 +3,96 @@ package com.grip.spark01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class signup extends AppCompatActivity {
 
+    TextInputLayout name,email,psw;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_signup);
+
+
+        name=findViewById(R.id.name1);
+        email=findViewById((R.id.email1));
+        psw=findViewById((R.id.psw1));
+
+
     }
+    private Boolean validateName(){
+            String n_val=name.getEditText().getText().toString();
+            if(n_val.isEmpty()){
+                psw.setError("Field cannot be empty");
+                return false;
+            }
+            else{
+                psw.setError(null);
+                return true;
+            }
+        }
+
+    private Boolean validateEmail(){
+            String e_val=email.getEditText().getText().toString();
+            String pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+            if(e_val.isEmpty()){
+                email.setError("Field cannot be empty");
+                return false;
+            }
+            else if(e_val.matches(pattern)){
+                email.setError("Invalid email address");
+                return false;
+            }
+            else{
+                email.setError(null);
+                return true;
+            }
+        }
+
+    private Boolean ValidatePSW(){
+        String p_val=email.getEditText().getText().toString();
+        String psw_val="^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+        if(p_val.isEmpty()){
+            email.setError("Field cannot be empty");
+            return false;
+        }
+        else if(p_val.matches(psw_val)){
+            email.setError("Invalid email address");
+            return false;
+        }
+        else{
+            email.setError(null);
+            return true;
+        }
+    }
+
+    public void HandleSignUp(View view){
+
+        if(!validateName() || !validateEmail() || ValidatePSW()){
+            return;
+        }
+
+        String user_name=name.getEditText().getText().toString();
+        String user_email=email.getEditText().getText().toString();
+        String user_psw=psw.getEditText().getText().toString();
+
+//        UserHelperClass helperClass=new UserHelperClass(user_name,user_email,user_psw);
+//        refer
+
+
+    }
+
 }
